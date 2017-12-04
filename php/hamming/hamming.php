9be1b2ff16ea0e5e7904b036e994f545795e7e6c
kevinwilde <kevinwilde2018@u.northwestern.edu>
2017-12-04 15:09:37 -0600

    Better version of hamming

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -2,13 +2,8 @@
 
 function distance(string $a, string $b) : int
 {
-    $len = strlen($a);
-    if (strlen($b) != $len)
+    if (strlen($a) != strlen($b))
         throw new InvalidArgumentException('DNA strands must be of equal length.');
 
-    $dist = 0;
-    for ($i = 0; $i < $len; $i++)
-        if ($a[$i] != $b[$i])
-            $dist++;
-    return $dist;
+    return count(array_diff_assoc(str_split($a), str_split($b)));
 }
