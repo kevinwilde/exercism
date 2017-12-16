64d527950b0d1eedac4f820b66fc11bf66fa3c01
kevinwilde <kevinwilde2018@u.northwestern.edu>
2017-12-15 22:18:15 -0600

    Refactor largest series product

--- php/largest-series-product/largest-series-product.php
+++ php/largest-series-product/largest-series-product.php
@@ -9,25 +9,27 @@ class Series
         if (is_int($s))
             $s = (string)$s;
 
-        if (!empty($s) && !ctype_digit($s))
+        if (empty($s)) {
+            $this->series = array();
+        } elseif (ctype_digit($s)) {
+            $toInt = function($e) { return (int)$e; };
+            $this->series = array_map($toInt, str_split($s));
+        } else {
             throw new InvalidArgumentException();
-
-        $this->series = $s;
+        }
     }
 
     public function largestProduct(int $n) : int
     {
-        if ($n < 0 || $n > strlen($this->series))
+        if ($n < 0 || $n > count($this->series))
             throw new InvalidArgumentException();
 
         if ($n === 0)
             return 1;
 
-        $toInt = function($e) { return (int)$e; };
-        $nums = array_map($toInt, str_split($this->series));
         $maxProd = 0;
-        for ($i = 0; $i <= strlen($this->series)-$n; $i++) {
-            $curProd = array_product(array_slice($nums, $i, $n));
+        for ($i = 0; $i <= count($this->series)-$n; $i++) {
+            $curProd = array_product(array_slice($this->series, $i, $n));
             $maxProd = max(array($maxProd, $curProd));
         }
         return $maxProd;
